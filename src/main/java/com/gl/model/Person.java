15bc64d50f14dd159a29850b3b3704cbfbc09e0b
Gitanjan123 <122338703+Gitanjan123@users.noreply.github.com>
2026-06-03 15:38:03 +0530

    Add webFLuxRouter - initial commit

---
+++ src/main/java/com/gl/model/Person.java
@@ -0,0 +1,26 @@
+package com.gl.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    private String id;
+    public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+	
